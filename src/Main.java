7a70619ba24f76b1ac1dd07d5454b5a5d424053a
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2018-06-05 00:58:44 +0900

    四則演算の学習（写経）

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,91 @@
 
+/**
+ * 対象文字列と文字位置を保持するクラス
+ *
+ */
 class Source {
-    public String str;
-    public int pos;
+    private final String str;
+    private int pos;
+
     public Source(String str) {
         this.str = str;
+    }
+
+    /**
+     * 現在の文字位置を返すメソッド
+     * @return
+     */
+    public final int peek() {
+        if(pos < str.length()) {
+            return str.charAt(pos);
+        }
+        return -1;
+    }
+
+    /**
+     * 文字位置を進める
+     */
+    public final void next() {
+        pos++;
+    }
+}
+
+class Parser extends Source {
+
+    public Parser(String str) {
+        super(str);
+    }
+
+    public final int number() {
+        StringBuilder sb = new StringBuilder();
+        int ch;
+        while((ch = peek()) >= 0 && Character.isDigit(ch)) {
+            sb.append((char) ch);
+            next();
+        }
+
+        return Integer.parseInt(sb.toString());
+
+    }
+
+    public final int expr() {
+        int x = number();
+        while(true) {
+            switch(peek()) {
+            case '+':
+                next();
+                x += number();
+                continue;
+            case '-':
+                next();
+                x -= number();
+                continue;
+            case '*':
+                next();
+                x *= number();
+                continue;
+            case '/':
+                next();
+                x /= number();
+                continue;
+            }
+            break;
+        }
+        return x;
     }
 }
 
 public class Main {
 
-    public static String number(Source s) {
-        for(int i = 0; i < s.str.length(); i++) {
-            s.pos = i;
-            if(!Character.isDigit(s.str.charAt(i))) {
-                return s.str.substring(0, i);
-            }
-        }
-        return s.str;
+    static void test(String s) {
+        System.out.println(s + " = " + new Parser(s).expr());
     }
+
     public static void main(String[] args) {
-        Source s = new Source("12+23+45");
-        System.out.println(number(s));
+       test("12+23+34");
+       test("56+23-34");
+       test("46/23*34");
+       test("46-23*2");
 
     }
 
